4da5c0b570eb9bb367be0921f59736f8a0192759
simrandhiman043 <simrandhiman621@gmail.com>
2026-07-29 21:20:06 +0530

    Created landing page

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,11 @@
 # Main Flask application
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>AI Smart Attendance Management System</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
