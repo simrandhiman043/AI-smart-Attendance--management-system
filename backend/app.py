cd77289af19db997062dfd38f2d718971764a5b6
simrandhiman043 <simrandhiman621@gmail.com>
2026-08-01 20:53:49 +0530

    made student and teacher cards clickable

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,20 @@
 # Main Flask application
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>AI Smart Attendance Management System</h1>"
+    return render_template("index.html")
+
+@app.route("/student-login")
+def student_login():
+    return render_template("student-login.html")
+
+@app.route("/teacher-login")
+def teacher_login():
+    return render_template("Teacher-login.html")    
 
 if __name__ == "__main__":
     app.run(debug=True)
+
