baf9cd8f41602d953eea8c4d96b525d82dbf22fb
simrandhiman043 <simrandhiman621@gmail.com>
2026-08-10 21:04:32 +0530

    Connect Flask to MySQL database

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,19 @@
 # Main Flask application
 from flask import Flask, render_template, request
+import mysql.connector
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+load_dotenv()  # Load environment variables from .env file
+
+db = mysql.connector.connect(
+    host=os.getenv("MYSQL_HOST"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    database=os.getenv("MYSQL_DATABASE"),
+    use_pure=True
+)
 
 @app.route("/")
 def home():
@@ -40,6 +52,8 @@ def student_dashboard():
 @app.route("/teacher-dashboard")
 def teacher_dashboard():
     return render_template("teacher-dashboard.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
